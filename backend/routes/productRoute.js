f4a2fe4b8badc4be34f407e6fbf832db97c8e100
Palak Parmar <palak7parmar@gmail.com>
2021-06-15 14:28:15 +0530

    adding error handling

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -36,7 +36,8 @@ router.get('/:id',asyncHandler(async(req,res)=>{
     res.json(product)
     }
     else{
-        res.status(404).json({message:'Product not found'})
+        res.status(404)
+        throw new Error('product not found')
     }
 }))
 
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoute.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 dotenv.config()
 connectDB()
@@ -11,26 +12,14 @@ const app=express()
 app.get('/',(req,res)=>{
     res.send('api is running')
 })
-// no in routes to not make this crowdy
-
-
-// app.get('/api/products',(req,res)=>{
-//     // can do re.send but its json so we have to do res.json
-//     res.json(products)
-// })
-
-// app.get('/api/products/:id',(req,res)=>{
-//     // getting a single product using id 
-//     const product = products.find((p)=>p._id === req.params.id)
-//     // can do re.send but its json so we have to do res.json
-//     // res.json(products)
-//     res.json(product)
-// })
-
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound);
+app.use(errorHandler);
+
 // if port not found use 5000
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 4200
 
 app.listen(
     PORT,
